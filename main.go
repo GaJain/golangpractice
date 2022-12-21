5b5a0abcd0b8beed2e2e0136d35c0e99bacca494
shivgaurani <gauravyup@gmail.com>
2022-12-21 19:44:38 +0530

    Added AddObjectAtIndex and DeleteObjectAtIndex

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+	sl := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(removeElementAtIndex(3, sl))
+
+	sl1 := []int{1, 2, 3, 4, 5}
+	fmt.Println(addElementAtIndex(2, sl1, 100))
+}
+
+// This function removes Object at Index of a Slice
+func removeElementAtIndex(index int, input []int) []int {
+	input = append(input[:index], input[index+1:]...)
+	return input
+}
+
+// This function Adds Object at Index of a Slice
+func addElementAtIndex(index int, input []int, value int) []int {
+	input = append(input[:index+1], input[index:]...)
+	input[index] = value
+	return input
+}
