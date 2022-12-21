44e31861c2459017f60e7629a5f3902add4535af
shivgaurani <gauravyup@gmail.com>
2022-12-21 20:13:33 +0530

    added repetetions method

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
 	sl := []int{1, 2, 3, 4, 5, 6, 7}
 	fmt.Println(removeElementAtIndex(3, sl))
 
 	sl1 := []int{1, 2, 3, 4, 5}
 	fmt.Println(addElementAtIndex(2, sl1, 100))
+
+	sl2 := "aaddbbeessaassddnnbbddg"
+	freq := repetetions(sl2)
+	fmt.Println("Frequency of Char is : ", freq)
 }
 
 // This function removes Object at Index of a Slice
@@ -21,4 +26,14 @@ func addElementAtIndex(index int, input []int, value int) []int {
 	input = append(input[:index+1], input[index:]...)
 	input[index] = value
 	return input
+}
+
+// This function tells Number of Repetetions of a char in a String
+func repetetions(input string) map[string]int {
+	freq := make(map[string]int)
+	for _, r := range input {
+		freq[string(r)] = freq[string(r)] + 1
+	}
+	return freq
+
 }
